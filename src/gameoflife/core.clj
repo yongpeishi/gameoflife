fbce0dfcc641bae95d76de21062309d181580d39
yongpeishi <yongpeishi@gmail.com>
2014-08-02 17:05:27 +1000

    refactor: not pass in size of world. use for loop instead of double map

--- src/gameoflife/core.clj
+++ src/gameoflife/core.clj
@@ -8,22 +8,12 @@
 ; Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
 ; FLOW
-; loop trough world
+; loop trough the given world
 ; get the collection of neighbours
 ; count neighbours that's alive
 ; decide if cell is alive on next generation
 ; draw world and update status of each cell (alive or not)
-
-(defn get-neighbours [world size x y]
-  (let [dxdy [[-1 -1] [0 -1] [1 -1]
-              [-1 0]         [1 0]
-              [-1 1]  [0 1]  [1 1]]]
-    (map (fn [[dx dy]]
-           (let [nx (+ x dx)
-                 ny (+ y dy)]
-             (if-not (or (neg? nx) (>= nx size) (neg? ny) (>= ny size))
-               (get-cell world nx ny))))
-         dxdy)))
+; repeat
 
 
 (defrecord Cell [x y alive-now alive-next])
@@ -31,57 +21,95 @@
 (defn get-cell [world x y]
   (nth (nth world y) x))
 
-(defn count-alive [neighbours]
-  (count
-   (filter (fn [cell]
-             (true? (:alive-now cell)))
-           neighbours)))
+(defn get-neighbours [world x y]
+  (let [size (count world)]
+    (for [[dx dy] [[-1 -1] [0 -1] [1 -1]
+                   [-1 0]         [1 0]
+                   [-1 1]  [0 1]  [1 1]]]
+      (let [nx (+ x dx)
+            ny (+ y dy)]
+        (if-not (or (neg? nx) (>= nx size) (neg? ny) (>= ny size))
+          (get-cell world nx ny))))))
 
-(defn alive-next? [lively-neighbours alive-now]
-  (or (== lively-neighbours 3)
-      (and (== lively-neighbours 2)
+(defn count-alive [neighbours]
+  (count (filter :alive-now neighbours)))
+
+(defn alive-next? [num-alive-neighbours alive-now]
+  (or (== num-alive-neighbours 3)
+      (and (== num-alive-neighbours 2)
            alive-now)))
 
-(defn cal-next-gen [world size]
+(defn cal-next-gen [world]
   (map (fn [row]
          (map (fn [cell]
-             (let [neighbours (get-neighbours world size (:x cell) (:y cell))]
+             (let [neighbours (get-neighbours world (:x cell) (:y cell))]
                (assoc cell :alive-next (alive-next? (count-alive neighbours) (:alive-now cell)))
                ))
            row))
        world))
 
-(defn draw [world]
+
+(defn new-world [calculated-gen]
+  (println calculated-gen)
   (map (fn [row]
          (map (fn [cell]
-                (let [alive (:alive-next cell)]
-                  (if alive
-                    (print "#")
-                    (print " ")))
-                (assoc cell :alive-now (:alive-next cell) :alive-next nil))
-               row))
-       world))
+             ;; draw the cell here
+             (assoc cell :alive-now (:alive-next cell) :alive-next nil))
+           row))
+       calculated-gen))
 
-(comment
+(defn next-gen [world]
+  (new-world (cal-next-gen world)))
 
-(alive-next? 0 true)
-(alive-next? 1 true)
-(alive-next? 2 true)
-(alive-next? 3 true)
-(alive-next? 4 true)
-(alive-next? 5 true)
-(alive-next? 6 true)
-(alive-next? 7 true)
-(alive-next? 8 true)
-(alive-next? 0 false)
-(alive-next? 1 false)
-(alive-next? 2 false)
-(alive-next? 3 false)
-(alive-next? 4 false)
-(alive-next? 5 false)
-(alive-next? 6 false)
-(alive-next? 7 false)
-(alive-next? 8 false)
+
+;; -----------------------------------------------------------------
+
+(def c00 (Cell. 0 0 false nil))
+(def c10 (Cell. 1 0 true  nil))
+(def c20 (Cell. 2 0 false nil))
+
+(def c01 (Cell. 0 1 false nil))
+(def c11 (Cell. 1 1 true nil))
+(def c21 (Cell. 2 1 false nil))
+
+(def c02 (Cell. 0 2 false nil))
+(def c12 (Cell. 1 2 true nil))
+(def c22 (Cell. 2 2 false nil))
+
+(def vertical-world [[c00 c10 c20]
+            [c01 c11 c21]
+            [c02 c12 c22]])
+
+
+(def c00 (Cell. 0 0 false nil))
+(def c10 (Cell. 1 0 false  nil))
+(def c20 (Cell. 2 0 false nil))
+
+(def c01 (Cell. 0 1 true nil))
+(def c11 (Cell. 1 1 true nil))
+(def c21 (Cell. 2 1 true nil))
+
+(def c02 (Cell. 0 2 false nil))
+(def c12 (Cell. 1 2 false nil))
+(def c22 (Cell. 2 2 false nil))
+
+(def horizontal-world [[c00 c10 c20]
+            [c01 c11 c21]
+            [c02 c12 c22]])
+
+
+(= (take 10 (cycle [vertical-world horizontal-world]))
+   (take 10 (iterate next-gen vertical-world)))
+
+(defn -main []
+  (take 10 (iterate next-gen vertical-world))
+
+
+;; -----------------------------------------------------------------
+
+
+;(comment
+; all the test, should be moved to test file
 
 (def cell-a (Cell. nil nil false true))
 (def cell-b (Cell. nil nil true true))
@@ -92,13 +120,15 @@
 (count-alive [cell-a cell-b nil cell-c cell-d nil])
 
 (def world-a [[1 2 3 4]
-            [5 6 7 8]
-            [9 10 11 12]
-            [13 14 15 16]])
+              [5 6 7 8]
+              [9 10 11 12]
+              [13 14 15 16]])
 
-(get-neighbours world-a 4 1 1)
-(get-neighbours world-a 4 0 0)
-(get-neighbours world-a 4 3 3)
+(get-neighbours world-a 1 1)
+(get-neighbours world-a 0 0)
+(get-neighbours world-a 3 3)
+
+(comment
 
 (def a (Cell. 0 0 false nil))
 (def b (Cell. 1 0 false nil))
@@ -113,20 +143,9 @@
 (def i (Cell. 2 2 false nil))
 
 
-(def world-b [[a b c]
-              [d e f]
-              [g h i]])
-
-(def new-world (cal-next-gen world-b 3))
-
-new-world
-
-world-b
-
-
-(def world-drawn (draw new-world))
-
-world-drawn
+(def world [[a b c]
+            [d e f]
+            [g h i]])
 
 
 )
--- test/gameoflife/core_test.clj
+++ test/gameoflife/core_test.clj
@@ -2,6 +2,61 @@
   (:require [clojure.test :refer :all]
             [gameoflife.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;alive-next?
+
+(deftest should-not-alive-next-when-0-neighbour-and-alive
+  (is (false? (alive-next? 0 true))))
+
+(deftest should-not-alive-next-when-1-neighbour-and-alive
+  (is (false? (alive-next? 1 true))))
+
+(deftest should-alive-next-when-2-neighbour-and-alive
+  (is (true? (alive-next? 2 true))))
+
+(deftest should-alive-next-when-3-neighbour-and-alive
+  (is (true? (alive-next? 3 true))))
+
+(deftest should-not-alive-next-when-4-neighbour-and-alive
+  (is (false? (alive-next? 4 true))))
+
+(deftest should-not-alive-next-when-5-neighbour-and-alive
+  (is (false? (alive-next? 5 true))))
+
+(deftest should-not-alive-next-when-6-neighbour-and-alive
+  (is (false? (alive-next? 6 true))))
+
+(deftest should-not-alive-next-when-7-neighbour-and-alive
+  (is (false? (alive-next? 7 true))))
+
+(deftest should-not-alive-next-when-8-neighbour-and-alive
+  (is (false? (alive-next? 8 true))))
+
+(deftest should-not-alive-next-when-0-neighbour-and-not-alive
+  (is (false? (alive-next? 0 false))))
+
+(deftest should-not-alive-next-when-1-neighbour-and-not-alive
+  (is (false? (alive-next? 1 false))))
+
+(deftest should-not-alive-next-when-2-neighbour-and-not-alive
+  (is (false? (alive-next? 2 false))))
+
+(deftest should-alive-next-when-3-neighbour-and-not-alive
+  (is (true? (alive-next? 3 false))))
+
+(deftest should-not-alive-next-when-4-neighbour-and-not-alive
+  (is (false? (alive-next? 4 false))))
+
+(deftest should-not-alive-next-when-5-neighbour-and-not-alive
+  (is (false? (alive-next? 5 false))))
+
+(deftest should-not-alive-next-when-6-neighbour-and-not-alive
+  (is (false? (alive-next? 6 false))))
+
+(deftest should-not-alive-next-when-7-neighbour-and-not-alive
+  (is (false? (alive-next? 7 false))))
+
+(deftest should-not-alive-next-when-8-neighbour-and-not-alive
+  (is (false? (alive-next? 8 false))))
+
+
+;
