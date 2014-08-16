2d7950fab75eb72c8cbbfdbd0d8c95a7a049fca8
yongpeishi <yongpeishi@gmail.com>
2014-08-16 12:03:29 +1000

    move test into test file

--- test/gameoflife/core_test.clj
+++ test/gameoflife/core_test.clj
@@ -57,6 +57,42 @@
 
 (deftest should-not-alive-next-when-8-neighbour-and-not-alive
   (is (false? (alive-next? 8 false))))
+; all the test, should be moved to test file
+
+(comment
+(def cell-a (Cell. nil nil false true))
+(def cell-b (Cell. nil nil true true))
+(def cell-c (Cell. nil nil true false))
+(def cell-d (Cell. nil nil true false))
+
+(count-alive [cell-a cell-b cell-c cell-d])
+(count-alive [cell-a cell-b nil cell-c cell-d nil])
+
+(def world-a [[1 2 3 4]
+              [5 6 7 8]
+              [9 10 11 12]
+              [13 14 15 16]])
+
+(get-neighbours world-a 1 1)
+(get-neighbours world-a 0 0)
+(get-neighbours world-a 3 3)
 
 
-;
+(def a (Cell. 0 0 false nil))
+(def b (Cell. 1 0 false nil))
+(def c (Cell. 2 0 false nil))
+
+(def d (Cell. 0 1 false nil))
+(def e (Cell. 1 1 true nil))
+(def f (Cell. 2 1 true nil))
+
+(def g (Cell. 0 2 false nil))
+(def h (Cell. 1 2 true nil))
+(def i (Cell. 2 2 false nil))
+
+
+(def world [[a b c]
+            [d e f]
+            [g h i]])
+
+)
